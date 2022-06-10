1c423bda6e78a3a8c8ec675091fdf179f700bcb7
Winme <winme0308@gmail.com>
2022-06-10 00:35:54 +0000

    chore: add axios request

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -1,15 +1,26 @@
-import Link from 'next/link'
+import axios from 'axios'
+import { GetStaticProps } from 'next'
 import Layout from '../components/Layout'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = (props) => {
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>Posts</h1>
+      <ul>
+        {
+          props.posts.map((post, index) => (<li key={index}>
+              <h3>{post.title}</h3>
+              <p>{post.content}</p>
+            </li>))
+        }
+      </ul>
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const post = await axios.get('http://localhost:3002/post')
+  return { props: { posts: post.data.posts } }
+}
 
 export default IndexPage
