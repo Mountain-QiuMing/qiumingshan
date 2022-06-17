9e135a5508d56c16d82b3e7465688a75d2f7b93b
Winme <winme0308@gmail.com>
2022-06-17 16:08:00 +0800

    fix: react 17 依赖

--- src/client/next.config.js
+++ src/client/next.config.js
@@ -1,7 +1,7 @@
 /** @type{import('next').NextConfig} */
 module.exports = {
   distDir: '../../.next',
-  compiler: {
+  experimental: {
     emotion: true,
   },
 };
--- src/client/pages/_app.tsx
+++ src/client/pages/_app.tsx
@@ -1,19 +1,20 @@
 import type { AppProps } from 'next/app';
-// import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { Global } from '@emotion/react';
 import globalStyle from '../style/global';
+import { colorModeConfig } from '../utils/color-mode';
 
-// const theme = extendTheme({
-//   config: colorModeConfig,
-// });
+const theme = extendTheme({
+  config: colorModeConfig,
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    // <ChakraProvider theme={theme}>
-    <>
-      <Global styles={globalStyle} />
-      <Component {...pageProps} />
-    </>
-    // </ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <>
+        <Global styles={globalStyle} />
+        <Component {...pageProps} />
+      </>
+    </ChakraProvider>
   );
 }
