b463ebfb0d2410d3fbbaa632282f8e4a20cbdbc0
Winme <winme0308@gmail.com>
2022-06-20 20:58:11 +0800

    feat: 添加 header nav 组件

---
+++ src/client/components/header.tsx
@@ -0,0 +1,43 @@
+import { FC } from 'react';
+import Image from 'next/image';
+import { css } from '@emotion/react';
+import { Box, Heading, Show } from '@chakra-ui/react';
+import Link from 'next/link';
+
+const menuList = [
+  { name: '首页', path: '/' },
+  { name: '问答', path: '/faq' },
+  { name: '发现', path: '/explore' },
+];
+
+const Header: FC = () => {
+  return (
+    <Box height={[16, 20]} px={[4, 20]} css={headerStyle}>
+      <Box display="flex" alignItems="center">
+        <Show above="sm">
+          <Image className="logo-img" src="/logo.png" width="40px" height="40px" />
+        </Show>
+
+        <Heading ml={1} fontSize="2xl">
+          秋名山
+        </Heading>
+      </Box>
+      <Box as="ul" pl={[4, 10]} display="flex" alignItems="center" className="menus">
+        {menuList.map(menu => (
+          <Box as="li" mr={[4, 10]} fontSize="large" key={menu.path}>
+            <Link href={menu.path}>{menu.name}</Link>
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default Header;
+
+const headerStyle = css`
+  display: flex;
+  .menus {
+    list-style: none;
+  }
+`;
--- src/client/pages/_app.tsx
+++ src/client/pages/_app.tsx
@@ -1,16 +1,18 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import { Global } from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
 import globalStyle from '../style/global';
 import { theme } from '../style/theme';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <>
-        <Global styles={globalStyle} />
-        <Component {...pageProps} />
-      </>
+      <ThemeProvider theme={theme}>
+        <>
+          <Global styles={globalStyle} />
+          <Component {...pageProps} />
+        </>
+      </ThemeProvider>
     </ChakraProvider>
   );
 }
