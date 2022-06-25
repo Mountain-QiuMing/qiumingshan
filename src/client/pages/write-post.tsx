13794691096d659871b202c82041cd027ce0b68f
Winme <winme0308@gmail.com>
2022-06-25 20:30:30 +0800

    feat: 写文章

--- src/client/pages/write-post.tsx
+++ src/client/pages/write-post.tsx
@@ -1,11 +1,20 @@
 import Editor from '@/components/editor';
-import Layout from '../src/components/layout';
+import { Box, Button, Center, Heading, Input } from '@chakra-ui/react';
+import Header from '../src/components/header';
 
 export default function WritePost() {
   return (
-    <Layout>
-      <h1>write post</h1>
-      <Editor />
-    </Layout>
+    <>
+      <Header justifyContent="space-between" px="10">
+        <Heading fontSize={['small', 'medium', 'large']}>写文章</Heading>
+        <Button colorScheme="primary">发布</Button>
+      </Header>
+      <Center>
+        <Box width="100%" maxWidth="1100px">
+          <Input placeholder="请输入标题" size="lg" />
+          <Editor />
+        </Box>
+      </Center>
+    </>
   );
 }
