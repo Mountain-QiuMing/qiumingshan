082a6926d02dc7f21463bf98b931c742fb4ff670
Winme <winme0308@gmail.com>
2022-06-27 20:41:51 +0800

    chore: view 模块目录迁移

--- src/server/server.module.ts
+++ src/server/server.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { AppModule } from './app.module';
-import { ViewModule } from 'src/server/view/view.module';
+import { ViewModule } from '@/modules/view/view.module';
 
 @Module({
   imports: [AppModule, ViewModule],
