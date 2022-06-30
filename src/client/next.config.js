964914ef1095c465e2fe8eb514999b4d31d968fb
Winme <winme0308@gmail.com>
2022-06-30 20:17:04 +0800

    🐞 fix: 页面重复渲染

--- src/client/next.config.js
+++ src/client/next.config.js
@@ -12,8 +12,7 @@ module.exports = {
       ...config.resolve.fallback,
       fs: false, // 处理浏览器端使用 dotenv fs 的依赖
     };
-
     return config;
   },
-  reactStrictMode: true,
+  // reactStrictMode: true, 会触发重复渲染 见 https://github.com/vercel/next.js/issues/35822
 };
