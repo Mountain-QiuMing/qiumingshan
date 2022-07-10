4369a4a95333e4e9218db1cc64f64c2450c24824
Winme <winme0308@gmail.com>
2022-07-10 17:30:48 +0800

    chore: 修改 404 ui

--- src/client/pages/404.tsx
+++ src/client/pages/404.tsx
@@ -1,21 +1,18 @@
-import { Button, Heading, Text, VStack } from '@chakra-ui/react';
+import { Button, Heading, Icon, Text, VStack } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { HomeFillIcon } from 'ultra-icon';
-import Layout from '@/components/layout';
 
 const NotFoundPage = () => {
   return (
-    <Layout>
-      <VStack justify="center" spacing="4" as="section" mt={['20', null, '40']} textAlign="center">
-        <Heading>404</Heading>
-        <Text fontSize={{ md: 'xl' }}>您要找的页面不存在</Text>
-        <NextLink href="/" passHref>
-          <Button as="a" aria-label="Back to Home" leftIcon={<HomeFillIcon />} colorScheme="teal" size="lg">
-            返回首页
-          </Button>
-        </NextLink>
-      </VStack>
-    </Layout>
+    <VStack justify="center" spacing="4" as="section" mt={['20', null, '40']} textAlign="center">
+      <Heading>404</Heading>
+      <Text fontSize={{ md: 'xl' }}>您要找的页面不存在</Text>
+      <NextLink href="/" passHref>
+        <Button as="a" aria-label="Back to Home" leftIcon={<Icon as={HomeFillIcon} />} colorScheme="teal" size="lg">
+          返回首页
+        </Button>
+      </NextLink>
+    </VStack>
   );
 };
 
